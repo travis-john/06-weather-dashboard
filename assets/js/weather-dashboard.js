38aafd3dce180e7e9adfebb43255785fae577dbb
travis-john <travis.wl.john@gmail.com>
2019-12-13 16:45:25 -0500

    commenting code

--- assets/js/weather-dashboard.js
+++ assets/js/weather-dashboard.js
@@ -10,55 +10,83 @@ var windSpeed = $('#wind-speed');
 var UVIndex = $('#uv-index');
 
 function renderWeather(cityName) {
+  // 5 Day Forecast API URL
   var forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&APPID=" + APIKey;
+
+  // Current Westher API URL
   var weatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&APPID=" + APIKey;
 
+//main AJAX Call - for weather
   $.ajax({
     url: weatherQueryURL,
     method: 'GET'
   }).then(function(response){
-    // console.log(response);
+
+    console.log(response);
+
+    // Storing Latitude and Longitude as variables for UV Index AJAX Call
     var Lat = "lat=" + response.coord.lat;
     var Lon = "lon=" + response.coord.lon;
-    UVQUeryURL = "https://api.openweathermap.org/data/2.5/uvi?" + "&APPID=" + APIKey + "&" + Lat + "&" + Lon;
-    // var currentWeatherIcon = response.list[3].weather[0].icon
-    // console.log(response);
 
+    // UV Index Query URL
+    UVQUeryURL = "https://api.openweathermap.org/data/2.5/uvi?" + "&APPID=" + APIKey + "&" + Lat + "&" + Lon;
+
+    // UV Index AJAX Call
     $.ajax({
       url: UVQUeryURL,
       method: 'GET'
     }).then(function(UVResponse){
-      // console.log(UVResponse);
+
+      console.log(UVResponse);
+
+      // Rendering UV Index from AJAX Call
       UVIndex.text('UV Index: ' + UVResponse.value);
     });
 
+    // 5 Day Forecast AJAX Call
+    $.ajax({
+      url: forecastQueryURL,
+      method: 'GET'
+    }).then(function(forecastResponse){
+
+      console.log(forecastResponse);
+
+      // renderForecast(forecastResponse);
+    });
+
+    // emptying out text fields on every submit
     city.empty();
     temp.empty();
     humidity.empty();
     windSpeed.empty();
 
-
+    // filling out text fields based on API Call Results
     city.text(response.name);
-    // weatherIcon.attr('src', "https://openweathermap.org/img/wn/" + currentWeatherIcon + "@2x.png" )
     temp.text("Temperature: " + response.main.temp + "℉");
     humidity.text("Humidity: " + response.main.humidity + "%");
     windSpeed.text("Wind Speed: " + response.wind.speed + "MPH");
 
   });
+
+
 }
 
 $('.btn-primary').click(function(event){
   event.preventDefault;
+
+  // defining userInput for AJAX Call
   var userInput = $('#city-search').val().trim();
+
+  // accounting for blank input field and invalid response
   if((userInput === null) || (userInput === '')){
     alert("Please enter a valid city");
-  } else {
+  }
+
+  // storing UserInput, pushing to array and adding to localStorage
+  //running AJAX call function on submit
+  else {
     userSearch.push(userInput);
     localStorage.setItem("city-searches", JSON.stringify(userSearch));
     renderWeather(userInput);
-    // userInput.empty();
   }
-
-
-
 });
