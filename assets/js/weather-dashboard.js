a9e002f5774404e1656bed3aa0c1b4735bd242ca
travis-john <travis.wl.john@gmail.com>
2019-12-13 15:47:25 -0500

    attempting to render weather

--- assets/js/weather-dashboard.js
+++ assets/js/weather-dashboard.js
@@ -2,9 +2,59 @@
 var APIKey = '950e5f6ee11a03024c303d695a0203f1';
 var userSearch = localStorage.getItem('city-searches');
 userSearch = JSON.parse(userSearch) || [];
-var userSearch = $('#city-search').val().trim();
+var userInput = $('#city-search').val().trim();
+var city = $('#city-name');
+var weatherIcon = $('#weather-icon');
+var temp = $('#temperature');
+var humidity = $('#humidity');
+var windSpeed = $('#wind-speed');
+var UVIndex = $('#uv-index');
 
 function renderWeather(cityName) {
   var forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&APPID=" + APIKey;
   var weatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&APPID=" + APIKey;
+
+  $.ajax({
+    url: weatherQueryURL,
+    method: 'GET'
+  }).then(function(response){
+    // console.log(response);
+    var Lat = "lat=" + response.coord.lat;
+    var Lon = "lon=" + response.coord.lon;
+    UVQUeryURL = "https://api.openweathermap.org/data/2.5/uvi?" + "&APPID=" + APIKey + "&" + Lat + "&" + Lon;
+    console.log(response);
+
+    $.ajax({
+      url: UVQUeryURL,
+      method: 'GET'
+    }).then(function(UVResponse){
+      console.log(UVResponse);
+      UVIndex.text('UV Index: ' + UVResponse.value);
+    });
+
+    city.empty();
+    temp.empty();
+    humidity.empty();
+    windSpeed.empty();
+
+
+    city.text(response.name);
+    temp.text("Temperature: " + response.main.temp + "℉");
+    humidity.text("Humidity: " + response.main.humidity + "%");
+    windSpeed.text("Wind Speed: " + response.wind.speed + "MPH");
+
+  });
 }
+
+$('.btn-primary').click(function(event){
+  event.preventDefault;
+  // if((userInput === null) || (userInput === '')){
+  //   alert("Please enter a valid city");
+  // } else {
+  //
+  // }
+
+  userSearch.push(userInput);
+  localStorage.setItem("city-searches", JSON.stringify(userSearch));
+  renderWeather(userInput);
+});
